4dfb6280cbc64f33217f65658135d570f57afbb2
Julian Chin A Foeng <julianchinafoeng@Julians-MacBook-Air.local>
2025-01-19 12:29:49 +0100

    FIlter blackpoint: parameter adjustment to tonecurve

--- Sources/BrightroomEngine/Filter/FilterBlackPoint.swift
+++ Sources/BrightroomEngine/Filter/FilterBlackPoint.swift
@@ -18,16 +18,29 @@ public struct FilterBlackPoint : Filtering, Equatable, Codable {
       
   }
   
-  public func apply(to image: CIImage, sourceImage: CIImage) -> CIImage {
-    let adjustedColor = CIColor(red: value, green: value, blue: value)
-    return
-      image
-        .applyingFilter(
-          "CIWhitePointAdjust",
-          parameters: [
-            "inputColor": adjustedColor
-          ]
-    )
-  }
+    public func apply(to image: CIImage, sourceImage: CIImage) -> CIImage {
+            // Map `value` to a tone curve adjustment
+            let blackPoint = CGFloat(value)
+            
+            // Define the tone curve points
+            let toneCurvePoints: [CIVector] = [
+                CIVector(x: 0, y: blackPoint),  // Adjust the bottom-left corner of the curve
+                CIVector(x: 0.25, y: 0.25 + blackPoint * 0.1),
+                CIVector(x: 0.5, y: 0.5 + blackPoint * 0.1),
+                CIVector(x: 0.75, y: 0.75 + blackPoint * 0.1),
+                CIVector(x: 1, y: 1)           // Keep the top-right corner unchanged
+            ]
+            
+            return image.applyingFilter(
+                "CIToneCurve",
+                parameters: [
+                    "inputPoint0": toneCurvePoints[0],
+                    "inputPoint1": toneCurvePoints[1],
+                    "inputPoint2": toneCurvePoints[2],
+                    "inputPoint3": toneCurvePoints[3],
+                    "inputPoint4": toneCurvePoints[4]
+                ]
+            )
+        }
   
 }
